7db9469460d61ff4d8e0c92d3ed4180ad22dd453
AlbertoBenaiges <albertobenaiges@gmail.com>
2024-02-15 17:43:12 +0100

    cambios 15-02-2024

---
+++ equationGenerationTest.py
@@ -0,0 +1,149 @@
+# File: equationGenerationTests.py
+# from chapter 14 of _Genetic Algorithms with Python_, an ebook
+# available for purchase at http://leanpub.com/genetic_algorithms_with_python
+#
+# Author: Clinton Sheppard <fluentcoder@gmail.com>
+# Repository: https://drive.google.com/open?id=0B2tHXnhOFnVkSm1DVWJiSzY5RTQ
+# Copyright (c) 2016 Clinton Sheppard
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import unittest
+import datetime
+import genetic13
+import random
+
+def evaluate(genes, prioritizedOperations):
+    equation = genes[:]
+    for operationSet in prioritizedOperations:
+        iOffset = 0
+    
+    for i in range(1, len(equation), 2):
+        i += iOffset
+        opToken = equation[i]
+        if opToken in operationSet:
+            leftOperand = equation[i - 1]
+            rightOperand = equation[i + 1]
+            equation[i - 1] = operationSet[opToken](leftOperand, rightOperand)
+            del equation[i + 1]
+            del equation[i]
+            iOffset += -2
+    return equation[0]
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def get_fitness(genes, expectedTotal, fnEvaluate):
+    result = fnEvaluate(genes)
+
+    if result != expectedTotal:
+        fitness = expectedTotal - abs(result - expectedTotal)
+    else:
+        fitness = 1000 - len(genes)
+    return fitness
+
+def display(genes, startTime, fitness):
+    timeDiff = datetime.datetime.now() - startTime
+    print("{0}\t{1}\t{2}".format((' '.join(map(str, [i for i in genes]))),fitness, timeDiff))
+    
+def create(numbers, operations, minNumbers, maxNumbers):
+    genes = [random.choice(numbers)]
+    count = random.randint(minNumbers, 1 + maxNumbers)
+    while count > 1:
+        count -= 1
+        genes.append(random.choice(operations))
+        genes.append(random.choice(numbers))
+    return genes
+
+def mutate(genes, numbers, operations, minNumbers, maxNumbers, fnGetFitness):
+    count = random.randint(1, 10)
+    initialFitness = fnGetFitness(genes)
+    while count > 0:
+        count -= 1
+
+        if fnGetFitness(genes) > initialFitness:
+            return
+        numberCount = (1 + len(genes)) / 2
+        appending = numberCount < maxNumbers and random.randint(0, 100) == 0
+
+        if appending:
+            genes.append(random.choice(operations))
+            genes.append(random.choice(numbers))
+            continue
+
+        removing = numberCount > minNumbers and random.randint(0, 20) == 0
+        
+        if removing:
+            index = random.randrange(0, len(genes) - 1)
+            del genes[index]
+            del genes[index]
+            continue
+
+        index = random.randrange(0, len(genes))
+        genes[index] = random.choice(operations) if (index & 1) == 1 else random.choice(numbers)
+
+class EquationGenerationTests(unittest.TestCase):
+
+    def test_addition(self):
+        operations = ['+', '-']
+        prioritizedOperations = [{'+': add,'-': subtract}]
+        optimalLengthSolution = [7, '+', 7, '+', 7, '+', 7, '+', 7, '-', 6]
+        self.solve(operations, prioritizedOperations, optimalLengthSolution)
+
+    def test_multiplication(self):
+        operations = ['+', '-', '*']
+        prioritizedOperations = [{'*': multiply},{'+': add,'-': subtract}]
+        optimalLengthSolution = [6, '*', 3, '*', 3, '*', 6, '-', 7]
+        self.solve(operations, prioritizedOperations, optimalLengthSolution)
+        
+    def test_exponent(self):
+        operations = ['^', '+', '-', '*']
+        prioritizedOperations = [{'^': lambda a, b: a ** b},
+                                {'*': multiply},
+                                {'+': add,
+                                '-': subtract}]
+        optimalLengthSolution = [6, '^', 3, '*', 2, '-', 5]
+        self.solve(operations, prioritizedOperations, optimalLengthSolution)
+
+    def solve(self, operations, prioritizedOperations, optimalLengthSolution):
+        numbers = [1, 2, 3, 4, 5, 6, 7]
+        expectedTotal = evaluate(optimalLengthSolution, prioritizedOperations)
+        minNumbers = (1 + len(optimalLengthSolution)) / 2
+        maxNumbers = 6 * minNumbers
+        startTime = datetime.datetime.now()
+
+        def fnDisplay(genes):
+            display(genes, startTime)
+        
+        def fnEvaluate(genes):
+            return evaluate(genes, prioritizedOperations)
+        
+        def fnGetFitness(genes):
+            return get_fitness(genes, expectedTotal, fnEvaluate)
+        
+        def fnCreate():
+            return create(numbers, operations, minNumbers, maxNumbers)
+        
+        def fnMutate(child):
+            mutate(child, numbers, operations, minNumbers, maxNumbers, fnGetFitness)
+            optimalFitness = fnGetFitness(optimalLengthSolution)
+            best = genetic13.get_best(fnGetFitness, None, optimalFitness, None, fnDisplay, fnMutate, fnCreate, maxAge=50)
+            self.assertTrue(not optimalFitness > best.Fitness)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ equationGenerationTest2.py
@@ -0,0 +1,39 @@
+import unittest
+import datetime
+import genetic13
+import random
+
+def evaluate(genes, prioritizedOperations):
+    results = genes[0]
+    
+    for i in range(1, len(genes), 2):
+        operation = genes[i]
+        nextValue = genes[i + 1]
+        if operation == '+':
+            result += nextValue
+        elif operation == '-':
+            result -= nextValue
+    return result
+
+class EquationGenerationTests(unittest.TestCase):
+    def test(self):
+        numbers = [1,2,3,4,5,6,7]
+        operations = ['+','-']
+
+def create(numbers, operations, minNumbers, maxNumbers):
+    genes = [random.choice(numbers)]
+    count = random.randint(minNumbers, 1 + maxNumbers)
+    while count > 1:
+        count -= 1
+        genes.append(random.choice(operations))
+        genes.append(random.choice(numbers))
+    return genes
+
+def mutate(genes, numbers, operations, minNumbers, maxNumbers):
+    numberCount = (1 +len(genes))/2
+    appending = numberCount < maxNumbers and random.randint(0,100) == 0
+
+    if appending:
+        genes.append(random.choice(operations))
+        genes.append(random.choice(numbers))
+        return
